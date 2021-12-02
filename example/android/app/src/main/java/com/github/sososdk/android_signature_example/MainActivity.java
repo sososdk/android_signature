26c0162b8f7b94871c8a70cbb5564ac032ede7f8
sososdk <csshuai2009@gmail.com>
2021-12-02 10:31:29 +0800

    Initial commit.

---
+++ example/android/app/src/main/java/com/github/sososdk/android_signature_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.github.sososdk.android_signature_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import android_signature
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AndroidSignaturePlugin.register(with: registry.registrar(forPlugin: "AndroidSignaturePlugin"))
+}
